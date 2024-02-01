f2c3e62ba8fdb14c56d90149eafcf744c3fd2331
@Rgoes06-1 <renangoesjunior@outlook.com>
2024-02-01 15:55:18 -0300

    Sprint-06

---
+++ sprint-06/exercicios/ExerciciosSprint06.java
@@ -0,0 +1,40 @@
+public class ExerciciosSprint06 {
+    public static void main(String[] args) {
+        // Exercício 01
+        System.out.println("Seja bem vindo ao COMEX!");
+
+        //Exercício 04 e 06
+
+        String nome = "Fulano";
+        int dataDeNascimento = 1980;
+
+        String nome2 = "Sicrano";
+        int dataDeNascimento2 = 2000;
+
+        System.out.println(">>> Listagem dos Clientes");
+        System.out.println("_____________________________");
+        System.out.println("Nome:" + nome);
+        System.out.println("Data de Nascimento:" + dataDeNascimento);
+        System.out.println("_____________________________");
+        System.out.println("Nome:" + nome2);
+        System.out.println("Data de Nascimento:" + dataDeNascimento2);
+
+        // Exercício 05
+
+        String nomeLivro = "Livro";
+        double precoLivro = 49.95;
+
+        String nomeCelular = "Celular";
+        double precoCelular = 1470.76;
+
+        System.out.println(">>> Listagem dos Produtos");
+        System.out.println("_____________________________");
+        System.out.println("Nome:" + nomeLivro);
+        System.out.println("Preço:R$" + precoLivro);
+        System.out.println("_____________________________");
+        System.out.println("Nome:" + nomeCelular);
+        System.out.println("Preço:R$" + precoCelular);
+
+
+    }
+}
